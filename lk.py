539e08c2425e9e40899780ad8676800b2d2790ec
youssefhassan01 <youssef.hassan01@eng-st.cu.edu.eg>
2023-12-24 01:39:33 +0200

    Viola-Jones + KLT works
    i think?

--- lk.py
+++ lk.py
@@ -1,100 +1,34 @@
-# import numpy as np
-# import cv2
-
-
-
-
-# # Create some random colors
-# def klt(old_frame, new_frame, feature_params, lk_params):
-#     color = [0, 255, 0]
-
-
-
-#     # Create a mask image for drawing purposes
-#     mask = np.zeros_like(old_frame)
-
-
-#     p0 = cv2.goodFeaturesToTrack(old_frame, mask=None,
-#                                 **feature_params)
-    
-#     # calculate optical flow
-#     p1, st, err = cv2.calcOpticalFlowPyrLK(old_frame,
-#                                            new_frame,
-#                                            p0, None,
-#                                            **lk_params)
-
-#     # Select good points
-#     good_new = p1[st == 1]
-#     good_old = p0[st == 1]
-
-#     # draw the tracks
-#     for i, (new, old) in enumerate(zip(good_new, good_old)):
-#         a, b = new.ravel().astype(int)
-#         c, d = old.ravel().astype(int)
-#         mask = cv2.line(mask, (a, b), (c, d),
-#                         color[i].tolist(), 2)
-
-#         frame = cv2.circle(frame, (a, b), 5, color[i].tolist(), -1)
-
-#     img = cv2.add(frame, mask)
-
-#     # Updating Previous frame and points
-#     old_frame = new_frame.copy()
-#     p0 = good_new.reshape(-1, 1, 2)
-
-
+import numpy as np
 import cv2
 
 
-def track_features(frame):
+# Take first frame and find corners in it
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# Create a mask image for drawing purposes
 
-    # Calculate optical flow
-    new_points, status, error = cv2.calcOpticalFlowPyrLK(prev_gray, gray, feature_points, None, **lk_params)
+
+def klt(frame_gray,old_gray,p0,lk_params):
+    # calculate optical flow
+    p1, st, err = cv2.calcOpticalFlowPyrLK(old_gray,
+                                           frame_gray,
+                                           p0, None,
+                                           **lk_params)
 
     # Select good points
-    good_new = new_points[status == 1]
-    good_old = feature_points[status == 1]
-
-    # Draw the tracks
+    good_new = p1[st == 1]
+    good_old = p0[st == 1]
     
+    # draw the tracks
     for i, (new, old) in enumerate(zip(good_new, good_old)):
-        a, b = new.ravel()
-        c, d = old.ravel()
-        mask = cv2.line(mask, (a, b), (c, d), color=(0, 255, 0), thickness=2)
-        frame = cv2.circle(frame, (a, b), 5, color=(0, 255, 0), thickness=-1)
+        a, b = new.ravel().astype(int)
+        c, d = old.ravel().astype(int)
+        cv2.line(frame_gray, (a, b), (c, d),
+                        (0, 255, 0), 2)
 
-    # Update mask and feature points
-    global prev_gray, feature_points
-    prev_gray = gray.copy()
-    feature_points = good_new.reshape(-1, 1, 2)
+        cv2.circle(frame_gray, (a, b), 5, (0, 0, 255), -1)
 
-    return frame
-
-# Initialize KLT parameters and video capture
-# lk_params = dict(winSize=(15, 15), maxLevel=2, criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 10, 0.03))
-# cap = cv2.VideoCapture('video.mp4')
-
-# ret, frame1 = cap.read()
-# prev_gray = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-# good_features_to_track = cv2.goodFeaturesToTrack(prev_gray, 200, 0.3, 7)
-# feature_points = np.int0(good_features_to_track)
-# mask = np.zeros_like(prev_gray)
-
-# # Outer loop for reading video
-# while True:
-#     ret, frame = cap.read()
-#     if not ret:
-#         break
-
-#     # Track features within the function
-#     tracked_frame = track_features(frame)
-
-#     cv2.imshow('Tracking', tracked_frame)
-
-#     if cv2.waitKey(1) == ord('q'):
-#         break
-
-# cap.release()
-# cv2.destroyAllWindows()
+    # Updating Previous frame and points
+    old_gray = frame_gray.copy()
+    p0 = good_new.reshape(-1, 1, 2)
+    
+    return frame_gray
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 from viola_jones import viola_jones
+from lk import klt
+
+
 
 def main():
     # KLT parameters
@@ -15,29 +18,40 @@ def main():
                  maxLevel=2,
                  criteria=(cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT,
                            10, 0.03))
+    prev_gray = None
+    feature_points = None
+    mask = None
     
     camera = cv2.VideoCapture(0)
+    _,first_frame = camera.read()
+    first_gray_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
     while True:
         _, frame = camera.read()
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         
         faces = viola_jones(gray)
-        
-        viola_jones_face = np.zeros(gray.shape)
-        
-        (x, y, width, height) = faces[0]
-        # cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 255, 0), 2)
-        viola_jones_face[y:y+height, x:x+width] = gray[y:y+height, x:x+width]
-        
-        feature_points = cv2.goodFeaturesToTrack(viola_jones_face, 200, 0.3, 7)
-        feature_points = np.int0(feature_points) + np.array([x, y])
-        
-        
-        cv2.imshow("faces", frame)
+        if len(faces) > 0:
+            (x, y, width, height) = faces[0]
+            # cv2.rectangle(frame, (x, y), (x+width, y+height), (0, 255, 0), 2)
+            viola_jones_face = gray[y:y+height, x:x+width]
+            
+            # feature_points = cv2.goodFeaturesToTrack(viola_jones_face, 200, 0.3, 7)
+            # feature_points = np.int0(feature_points) + np.array([x, y])
 
+            # Initialize feature points in the first frame or after face loss
+            feature_points = cv2.goodFeaturesToTrack(viola_jones_face, **feature_params)
+            feature_points = np.float32(feature_points) + np.float32(np.array([x, y]))
+
+
+            tracked_frame = klt(gray,first_gray_frame,feature_points,lk_params)
+
+        else:
+            tracked_frame = frame  # No faces detected, display original frame
+        
+        cv2.imshow('Tracking', tracked_frame)
+        # cv2.imshow("faces", frame)
         if (cv2.waitKey(1) == ord("q")):
             break
-        
     camera.release()
     cv2.destroyAllWindows()
 
