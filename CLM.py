45305a7c0f4b9d751723aa810cd64904f4694352
youssefhassan01 <youssef.hassan01@eng-st.cu.edu.eg>
2023-12-27 17:55:17 +0200

    Face filters

--- CLM.py
+++ CLM.py
@@ -1,41 +1,68 @@
-import dlib
 import cv2
+import numpy as np
+import dlib
+from math import hypot
 
+# Loading Camera and Nose image and Creating mask
+cap = cv2.VideoCapture(0)
+nose_image = cv2.imread("clown.png")
+_, frame = cap.read()
+rows, cols, _ = frame.shape
+nose_mask = np.zeros((rows, cols), np.uint8)
 
-predictor_path = './shape_predictor/shape_predictor_68_face_landmarks.dat'
-predictor = dlib.shape_predictor(predictor_path)
-
-camera = cv2.VideoCapture(0)
-
+# Loading Face detector
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 while True:
-    
+    _, frame = cap.read()
+    nose_mask.fill(0)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    _, frame = camera.read()
-    
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-
-    detector = dlib.get_frontal_face_detector()
-
-    # Detect faces
-    faces = detector(gray)
-
+    faces = detector(frame)
     for face in faces:
-        # Predict facial landmarks
-        landmarks = predictor(gray, face)
+        landmarks = predictor(gray_frame, face)
 
-        # Draw facial landmarks
-        for n in range(0, 68):
-            x = landmarks.part(n).x
-            y = landmarks.part(n).y
-            cv2.circle(frame, (x, y), 1, (0, 0, 255), -1)
+        # Nose coordinates
+        top_nose = (landmarks.part(29).x, landmarks.part(29).y)
+        center_nose = (landmarks.part(30).x, landmarks.part(30).y)
+        left_nose = (landmarks.part(31).x, landmarks.part(31).y)
+        right_nose = (landmarks.part(35).x, landmarks.part(35).y)
 
-    # Display the result
-    cv2.imshow('Facial Landmarks', frame)
+        nose_width = int(hypot(left_nose[0] - right_nose[0],
+                           left_nose[1] - right_nose[1]) * 1.7)
+        nose_height = int(nose_width * 0.77)
 
-    if (cv2.waitKey(1) == ord("q")):
-            break
+        # New nose position
+        top_left = (int(center_nose[0] - nose_width / 2),
+                              int(center_nose[1] - nose_height / 2))
+        bottom_right = (int(center_nose[0] + nose_width / 2),
+                       int(center_nose[1] + nose_height / 2))
 
-camera.release()
-cv2.destroyAllWindows()
+
+        # Adding the new nose
+        nose_pig = cv2.resize(nose_image, (nose_width, nose_height))
+        nose_pig_gray = cv2.cvtColor(nose_pig, cv2.COLOR_BGR2GRAY)
+        _, nose_mask = cv2.threshold(nose_pig_gray, 25, 255, cv2.THRESH_BINARY_INV)
+
+        nose_area = frame[top_left[1]: top_left[1] + nose_height,
+                    top_left[0]: top_left[0] + nose_width]
+        nose_area_no_nose = cv2.bitwise_and(nose_area, nose_area, mask=nose_mask)
+        final_nose = cv2.add(nose_area_no_nose, nose_pig)
+
+        frame[top_left[1]: top_left[1] + nose_height,
+                    top_left[0]: top_left[0] + nose_width] = final_nose
+
+        cv2.imshow("Nose area", nose_area)
+        cv2.imshow("Nose pig", nose_pig)
+        cv2.imshow("final nose", final_nose)
+
+
+
+    cv2.imshow("Frame", frame)
+
+
+
+    key = cv2.waitKey(1)
+    if key == 27:
+        break
